a67a478c8279768daaa3dbfb6fafcecd473241f0
extinctionsq <jasonmiracle56@gmail.com>
2019-03-18 15:05:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/timetolearn/RecyclerViewActivity.java
@@ -0,0 +1,40 @@
+package com.example.timetolearn;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+
+        initializeData();
+
+        RecyclerView rv = (RecyclerView)findViewById(R.id.rv);
+        rv.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+
+        RVAdapter adapter = new RVAdapter(movies);
+        rv.setAdapter(adapter);
+    }
+
+    private List<MovieData> movies;
+    private void initializeData(){
+        movies = new ArrayList<>();
+        movies.add(new MovieData("Jurassic World",
+                "https://m.media-amazon.com/images/M/MV5BNzQ3OTY4NjAtNzM5OS00N2ZhLWJlOWUtYzYwZjNmOWRiMzcyXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SY1000_CR0,0,631,1000_AL_.jpg","2015"));
+        movies.add(new MovieData("Jurassic World: Fallen Kingdom",
+                "https://m.media-amazon.com/images/M/MV5BNzIxMjYwNDEwN15BMl5BanBnXkFtZTgwMzk5MDI3NTM@._V1_.jpg","2018"));
+        movies.add(new MovieData("AntMan","https://m.media-amazon.com/images/M/MV5BMjM2NTQ5Mzc2M15BMl5BanBnXkFtZTgwNTcxMDI2NTE@._V1_.jpg","2015"));
+        movies.add(new MovieData("AntMan and The Wasp","https://m.media-amazon.com/images/M/MV5BYjcyYTk0N2YtMzc4ZC00Y2E0LWFkNDgtNjE1MzZmMGE1YjY1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SY1000_CR0,0,675,1000_AL_.jpg"
+                ,"2018"));
+    }
+}
